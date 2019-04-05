5241a0fd61fb1f35e6afb2411e38ebcc1e511b89
prestontewell <preston@tewellphoto.com>
2019-04-04 21:31:47 -0500

    This is a terrible commit but it was my first Vue.js app and I forgot. It's pretty much everything except a delete action. I'm a hot mess right now.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <!-- <h1>Products: {{ products }}</h1> -->
+    <p>Name: <input type="text" v-model ="newProductName"></p>
+    <p>Description: <input type="text" v-model ="newProductDescription"></p>
+    <p>Price: <input type="text" v-model ="newProductPrice"></p>
+    <button v-on:click="makeProduct()">Add New Product</button>
+
+    <div v-for="product in products">
+      <p>{{product.name}}</p>
+      <p>{{product.price}}</p>
+      <img v-bind:src="product.image_url">
+      <button v-on:click="currentProduct = product">Show more info</button>
+      <div v-if="product === currentProduct">
+        <p>{{product.description}}</p>
+        <p>Name: <input type="text" v-model="product.name"></p>
+        <p>Price: <input type="text" v-model="product.price"></p>
+        <p>Description: <input type="text" v-model="product.description"></p>
+        <button v-on:click="updateProduct(product)">Update Product</button>
+        <button v-on:click="deletProduct(product)">Delete Product</button>
+      </div>
+      </div>
+    </div>
+
   </div>
+
 </template>
+
+<style>
+  img {
+    width: 150 px;
+  }
+</style>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios"
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to The Store!",
+      products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      currentProduct: {}
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+    })
+  },
+
+methods: {
+  makeProduct: function() {
+    var params = {
+      name: this.newProductName,
+      description: this.newProductDescription,
+      price: this.newProductPrice,
+      image_url: this.newProductImageUrl
+    }
+
+    axios.post("/api/products", params).then(response => {
+      console.log(response);
+      this.products.push(response.data);
+    });
+  },
+  toggleInfo: function(theProduct) {
+    if (this.currentProduct === theProduct) {
+      this.currentProduct = {};
+    }
+  },
+  updateProduct: function(theProduct) {
+    console.log(theProduct)
+    var params = {
+      name: theProduct.name,
+      description: theProduct.description,
+      price: theProduct.price,
+      image_url: theProduct.image_url
+    };
   }
 }
+
+};
 </script>
